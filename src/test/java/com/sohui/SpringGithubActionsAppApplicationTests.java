d64cbccca5a9c08194e0d572d940a126a5216cc1
sohuigim <aisurudake@gmail.com>
2024-09-19 15:38:36 +0900

    first

---
+++ src/test/java/com/sohui/SpringGithubActionsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sohui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubActionsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
